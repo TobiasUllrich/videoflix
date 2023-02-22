875718335fae83732b8e5baf0e502852c8e8bf32
Tobias Ullrich <ullrich.tobias@gmx.de>
2023-02-22 23:39:50 +0100

    feb22-2

--- content/signals.py
+++ content/signals.py
@@ -1,7 +1,35 @@
+import os
 from .models import Video
+from content.tasks import convert_480p
+from django.dispatch import receiver
+from django.db.models.signals import post_save, post_delete
 
-#sender=Welche Instanz, instance=, created=True sobald erstellt, 
+@receiver(post_save, sender=Video)
 def video_post_save(sender, instance, created, **kwargs):
-    print('Video wurde gespeichert')
-    if created:
-      print('Video wurde erschaffen')
+  """
+  Video is saved to filesystem
+  """
+  print('Video was saved')
+  if created:
+    print('Pfad ',instance.video_file.path)
+    convert_480p(instance.video_file.path) #Video is converted to 480p when created
+    print('Video was created')
+
+
+@receiver(post_delete, sender=Video)
+def video_post_delete(sender, instance, using, **kwargs):
+  """
+  Video gets deleted from filesystem, if corresponding object is deleted
+  """
+  if instance.video_file:
+    if os.path.isfile(instance.video_file.path):
+      print(instance.video_file.path) #Path of the video_file
+      os.remove(instance.video_file.path) #Video gets deleted
+      print('Video was deleted')
+
+#Verknüpft Funktion mit Model und den Zeitpunkt der Ausführung
+#post_save (nach dem speichern)
+#post_delete (nach dem löschen)
+#pre_save (vor dem Speichern)
+#pre_delete (vor dem löschen)
+#post_save.connect(video_post_save, sender=Video)
---
+++ content/tasks.py
@@ -0,0 +1,19 @@
+import os
+import subprocess
+
+def convert_480p(source):
+
+  dir_path, file_name = os.path.split(source)  # Teile den Pfad und den Dateinamen auf
+  source='./' + file_name
+  name_only, ext = os.path.splitext(file_name)  # Teile den Dateinamen und die Erweiterung auf
+  new_name = name_only + '_480p' + ext  # Neuen Namen erstellen
+  target = os.path.join('./', new_name)  # Ziel-Pfad erstellen
+  print('Pfad', target)
+
+
+  cmd = 'ffmpeg -i "{}" -s hd480 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source,target) #Command
+  print('Command ', cmd)
+  subprocess.run(cmd, capture_output=True) #Run command
+
+  
+ 
