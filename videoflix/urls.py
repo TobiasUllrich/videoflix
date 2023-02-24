d7ddfc70f69b464d60545dbba9fe380fe917e8de
Tobias Ullrich <ullrich.tobias@gmx.de>
2023-02-24 08:45:11 +0100

    24feb

--- videoflix/urls.py
+++ videoflix/urls.py
@@ -23,3 +23,5 @@ urlpatterns = [
     path('__debug__/', include('debug_toolbar.urls')),
     path('django-rq/', include('django_rq.urls')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
+
